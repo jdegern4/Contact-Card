7515ae625f4ec8da5340719795659222945557fd
Jordan D <degco44@gmail.com>
2022-09-11 21:43:47 -0500

    add PWA install capability

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -8,6 +8,22 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
 window.addEventListener("load", function () {
   initDB();
   document.getElementById("logo").src = Logo;
